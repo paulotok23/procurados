d2cc3884a054c97acdffe4199d7c71e165599393
Paulo Henrique <54315756+paulotok23@users.noreply.github.com>
2020-10-01 11:12:50 -0300

    Add files via upload

--- esqueci.php
+++ esqueci.php
@@ -54,40 +54,23 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Login</div>
+                    <div class="card-header">Recuperar Senha</div>
                     <div class="card-body">
-                        <form action="login.php" method="POST">
+                        <form action="envia.php" method="POST">
                             <div class="form-group row">
-                                <label for="user" class="col-md-4 col-form-label text-md-right">Usuário</label>
+                                <label for="email" class="col-md-4 col-form-label text-md-right">Digite seu Email</label>
                                 <div class="col-md-6">
-                                    <input type="text" id="user" class="form-control" name="user" required autofocus>
+                                    <input type="text" id="email" class="form-control" name="email" required autofocus>
                                 </div>
                             </div>
 
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
-                                <div class="col-md-6">
-                                    <input type="password" id="password" class="form-control" name="password" required>
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember"> Lembrar-me
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
+                            
 
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Entrar
+                                    Enviar
                                 </button>
-                                <a href="esqueci.php" class="btn btn-link">
-                                    Esqueci minha senha?
-                                </a>
+                                
                             </div>
                     </div>
                     </form>
--- page-adm.php
+++ page-adm.php
@@ -57,39 +57,7 @@
                     <div class="card-header">Login</div>
                     <div class="card-body">
                         <form action="login.php" method="POST">
-                            <div class="form-group row">
-                                <label for="user" class="col-md-4 col-form-label text-md-right">Usuário</label>
-                                <div class="col-md-6">
-                                    <input type="text" id="user" class="form-control" name="user" required autofocus>
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
-                                <div class="col-md-6">
-                                    <input type="password" id="password" class="form-control" name="password" required>
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember"> Lembrar-me
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Entrar
-                                </button>
-                                <a href="esqueci.php" class="btn btn-link">
-                                    Esqueci minha senha?
-                                </a>
-                            </div>
-                    </div>
+                            
                     </form>
                 </div>
             </div>
